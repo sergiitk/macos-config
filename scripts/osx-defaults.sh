8cd5f5d13975237c8ea2e8ee7c2190837ead11fb
Sergii Tkachenko <hi@sergii.org>
2018-01-04 17:56:10 -0500

    Add more settings

--- scripts/osx-defaults.sh
+++ scripts/osx-defaults.sh
@@ -2,7 +2,13 @@
 
 # System Preferences
 killall "System Preferences" > /dev/null 2>&1
-defaults delete -g NSUserKeyEquivalents
+
+# System Preferences - General -> Appearance: Graphite
+defaults write -g AppleAquaColorVariant -int 6
+
+# System Preferences - Keyboard -> Key Repeat: Fast
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 # System Preferences - Keyboard -> Shortcuts -> App Shortcuts
 # ~/Library/Preferences/.GlobalPreferences.plist
@@ -12,6 +18,8 @@ defaults delete -g NSUserKeyEquivalents
 # $ for Shift
 # ~ for Option
 # ^ for Ctrl
+# Flush existing settings. Won't pick up updates without it.
+defaults delete -g NSUserKeyEquivalents
 # Next tab: cmd + opt + left-arrow
 defaults write -g NSUserKeyEquivalents -dict-add 'Show Next Tab' '@~\U2192'
 # Prev tab: cmd + opt + right-arrow
@@ -19,6 +27,11 @@ defaults write -g NSUserKeyEquivalents -dict-add 'Show Previous Tab' '@~\U2190'
 
 # Restart the Finder and the preferences daemon for the change to take effect
 killall Dock
+killall SystemUIServer
 killall Finder
 killall cfprefsd
-killall SystemUIServer
+
+
+# System Preferences - General -> Use dark menu bar and Dock
+# Doesn't work: defaults write -g AppleInterfaceStyle -string "Dark"
+osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
