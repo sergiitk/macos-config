cb7267bb1f70ff3209ba637bacfbaf1a9b0b8eea
Sergii Tkachenko <hi@sergii.org>
2018-01-04 18:57:00 -0500

    More stuff that don't work :(

--- scripts/osx-defaults.sh
+++ scripts/osx-defaults.sh
@@ -3,14 +3,21 @@
 # System Preferences
 killall "System Preferences" > /dev/null 2>&1
 
-# System Preferences - General -> Appearance: Graphite
+# General -> Appearance: Graphite
 defaults write -g AppleAquaColorVariant -int 6
 
-# System Preferences - Keyboard -> Key Repeat: Fast
+# Needs relogin.
+# Keyboard -> Key Repeat: Fast
 defaults write NSGlobalDomain KeyRepeat -int 2
+# Keyboard -> Delay Until Repeat: Short
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
-# System Preferences - Keyboard -> Shortcuts -> App Shortcuts
+# Keyboard -> Shortcuts -> Mission Control -> Mission Control: off
+# Doesn't work :(
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 32 "{ enabled = 0; value = { parameters = (65535, 126, 8650752); type = 'standard'; }; }"
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 34 "{ enabled = 0; value = { parameters = (65535, 126, 8781824); type = 'standard'; }; }"
+
+# Keyboard -> Shortcuts -> App Shortcuts
 # ~/Library/Preferences/.GlobalPreferences.plist
 # http://hints.macworld.com/article.php?story=20131123074223584
 # http://osxnotes.net/keybindings.html
@@ -26,12 +33,11 @@ defaults write -g NSUserKeyEquivalents -dict-add 'Show Next Tab' '@~\U2192'
 defaults write -g NSUserKeyEquivalents -dict-add 'Show Previous Tab' '@~\U2190'
 
 # Restart the Finder and the preferences daemon for the change to take effect
-killall Dock
-killall SystemUIServer
-killall Finder
 killall cfprefsd
+killall SystemUIServer
+killall Dock
+killall Finder
 
-
-# System Preferences - General -> Use dark menu bar and Dock
+# General -> Use dark menu bar and Dock
 # Doesn't work: defaults write -g AppleInterfaceStyle -string "Dark"
 osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
